4218ca9c2b2aefb2d97782cd1f286c40f1e0f496
Marcio Barrientos <marcio.barrientos@gmail.com>
2023-06-19 16:59:58 -0600

    Enlace a portada en logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,18 @@
 import { ChevronRightIcon } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-square">
-        <ChevronRightIcon strokeWidth={4} size={21} color="#1a2336" />
-      </div>
-      <div className="logo-text">
-        BANCO <br/>
-        PICHINCHA
-      </div>
+      <Link to='/' className="header-link">
+        <div className="logo-square">
+          <ChevronRightIcon strokeWidth={4} size={21} color="#1a2336" />
+        </div>
+        <div className="logo-text">
+          BANCO <br/>
+          PICHINCHA
+        </div>
+      </Link>
     </header>
   )
 }
